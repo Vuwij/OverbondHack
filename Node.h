a4388d80678e9053f78eee8ab2403da43fc7b7fa
dhruvchawla1996 <dhruvchawla1996@gmail.com>
2016-06-18 14:47:19 -0400

    Created Node.h

---
+++ Node.h
@@ -0,0 +1,20 @@
+/* 
+ * File:   Node.h
+ * Author: dhruvchawla
+ *
+ * Created on June 18, 2016, 2:41 PM
+ */
+
+using namespace std;
+
+#ifndef NODE_H
+#define	NODE_H
+
+class Node {
+public:
+    unsigned int id;
+    string type;
+};
+
+#endif	/* NODE_H */
+
