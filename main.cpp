7e43ba16a162b2cf266ce7743bd6c7da7d5fbafc
Jason Wang <vuwij@me.com>
2016-06-18 12:32:13 -0400

    Fixed starting files

--- main.cpp
+++ main.cpp
@@ -5,15 +5,14 @@
  * Created on June 18, 2016, 12:27 PM
  */
 
-#include <cstdlib>
+#include <iostream>
 
 using namespace std;
 
-/*
- * 
- */
 int main(int argc, char** argv) {
-
+    
+    cout << "Dhruv";
+    
     return 0;
 }
 
